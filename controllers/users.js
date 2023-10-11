8f8d8416e092bbcf5f4de5de77763e25ec407101
Giovanni-Peralta-Mercado <isc20350543@gmail.com>
2023-10-10 19:56:24 -0600

    first commit

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,26 @@
-const {request,response} = require('express')
-const connection = require('../db');
+const {request,response} = require('express');
+const usersModel = require('../models/users');
+const pool = require('../db');
 
-const usersList=(req = request,res=response)=>{
+const usersList= async(req = request,res=response)=>{
+    let conn;
     try {
-        connection.connect((err)=>{
-            if (err){
-                throw new Error(err);
-            }else{
-                connection.execute('SELECT * FROM users', (err,users) =>{
-                    if (err){
-                        throw new Error(err);
-                    }
-                    res.json(users);
-                })
+        conn = await pool.getConnection();
+        
+        const users = await conn.query(usersModel.getAll,(err)=>{
+            if(err){
+                throw new Error(err);   
             }
         })
+
+        res.json(users);
     } catch (error) {
-        res.status(500).json({msg: "Error connecting to MySQL database"});
+        console.log(error);
+        res.status(500).json(error);
         
     }finally{
-        connection.end();
+        if(conn) conn.end();
     }
-    
-    
 }
 
 module.exports={usersList};
